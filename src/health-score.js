c5e88922ddc97d6838c4f65863ac934b9211e2ec
Filip Maj <fmaj@slack-corp.com>
2024-05-09 14:30:12 -0400

    read gitignore to populate places to ignore

--- src/health-score.js
+++ src/health-score.js
@@ -1,28 +1,44 @@
 const child_process = require('child_process');
+const fs = require('fs');
 
 module.exports = {
   calc: function calculateScore(/* core */) {
     // TODO: wire up action inputs
     console.log('pwd:', child_process.execSync('pwd').toString());
     console.log('ls -al:', child_process.execSync('ls -al').toString());
-    return module.exports.grep('js', 'node_modules');
     // TODO: wire up action outputs
+    return module.exports.grep('js', ['src'], []);
   },
-  grep: function grepForProblematicComments(ext, ignore) {
-    let find = `find . -name "*.${ext}"`;
-    if (ignore) {
-      find += ` -not -path "*/${ignore}/*"`;
+  grep: function grepForProblematicComments(ext, include, exclude) {
+    let find = 'find';
+    if (include && include.length) {
+      include.forEach((i) => {
+        find += ` ${i}`;
+      });
+    } else {
+      find += ' .';
     }
+    find += ` -name "*.${ext}"`;
+    let ignores = [];
+    if (exclude && exclude.length) {
+      ignores = exclude;
+    } else if (fs.existsSync('.gitignore')) {
+      ignores = fs.readFileSync('.gitignore').toString().split('\n')
+        .filter(Boolean)
+        .map((entry) => entry.trim());
+    }
+    ignores.forEach((ex) => {
+      find += ` -not -path "*/${ex}/*" -not -path "*/${ex}"`;
+    });
     find += ' -exec grep -E \'TODO|HACK|FIXME\' {} \\;';
-    let count = 0;
     console.log(find);
+    let output;
     try {
-      const out = child_process.execSync(find).toString().trim();
-      console.log(out);
-      count = parseInt(out, 10);
+      output = child_process.execSync(find).toString().trim();
+      console.log(output);
     } catch (e) {
       // TODO: handle error
     }
-    return count;
+    return output.split('\n').filter(Boolean).length;
   },
 };
