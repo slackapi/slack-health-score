7a1633b618eed56c260e4106b23a85b176de6b1f
Filip Maj <fmaj@slack-corp.com>
2024-05-10 13:48:24 -0400

    derp

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ const github = require('@actions/github');
 const hs = require('./health-score');
 
 const startTime = new Date();
-const score = hs.compile(core);
+const score = hs.compile(core, github);
 hs.report(startTime, core, github, score).then(console.log);
