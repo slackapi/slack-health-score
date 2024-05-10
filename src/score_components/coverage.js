0dfafeea8845cb166d4b001a19f3942a9822211d
Filip Maj <fmaj@slack-corp.com>
2024-05-10 14:40:35 -0400

    retry w/ codecov api

--- src/score_components/coverage.js
+++ src/score_components/coverage.js
@@ -11,25 +11,44 @@ module.exports = async function retrieveCodeCoverage(core, github) {
   // See if we can get a coverage overview for this commit from codecov
   const codecovToken = core.getInput('codecov_token');
   let misses = 0;
+  let attempts = 1;
   if (codecovToken) {
     const ctx = github.context;
     codecov.auth(codecovToken);
-    try {
-      const coverage = await codecov.repos_commits_retrieve({
-        service: 'github',
-        owner_username: ctx.repo.owner,
-        repo_name: ctx.repo.repo,
-        commitid: getSHA(core, github),
-      });
-      core.info(`codecov api response: ${JSON.stringify(coverage, null, 2)}`);
-      if (coverage && coverage.data && coverage.data.totals && coverage.data.totals.misses) {
-        misses = coverage.data.totals.misses;
-        core.info(`${misses} uncovered lines according to codecov`);
+    while (attempts < 10) {
+      try {
+        core.info('Pinging codecov API for coverage data...');
+        const coverage = await codecov.repos_commits_retrieve({
+          service: 'github',
+          owner_username: ctx.repo.owner,
+          repo_name: ctx.repo.repo,
+          commitid: getSHA(core, github),
+        });
+        core.info(`codecov api response: ${JSON.stringify(coverage, null, 2)}`);
+        if (coverage && coverage.data) {
+          if (coverage.data.totals && coverage.data.totals.misses) {
+            misses = coverage.data.totals.misses;
+            core.info(`${misses} uncovered lines according to codecov`);
+          } else {
+            // if totals are missing, probably codecov has not compiled the coverage info yet; delay and try again.
+            core.info('No coverage details available yet; sleeping for 3 seconds.');
+            attempts += 1;
+            await sleep(3000);
+          }
+        } else {
+          // TODO: if there's no data, what do we do?
+        }
+      } catch (e) {
+        core.error('Failed to retrieve codecov commits');
+        core.error(e);
       }
-    } catch (e) {
-      core.error('Failed to retrieve codecov commits');
-      core.error(e);
     }
   }
   return misses;
 };
+
+function sleep(ms) {
+  return new Promise((res) => {
+    setTimeout(res, ms);
+  });
+}
