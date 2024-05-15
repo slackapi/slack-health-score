66a2cada6a90866557bd178f11ab046e522f6f0f
Fil Maj <fmaj@slack-corp.com>
2024-05-15 17:45:14 +0000

    Add basic unit tests (#20)
    * document some basics about access tokens in README
    
    * Add basic codecov yaml
    
    * rename test file appropriately
    
    * add basic test coverage for codecov score calculation

--- src/score_components/coverage.js
+++ src/score_components/coverage.js
@@ -7,7 +7,7 @@ const RETRY_DELAY = 10000;
  * @description Compiles the health score components
  * @param {import('@actions/core')} core `@actions/core` GitHub Actions core helper utility
  * @param {import('@actions/github')} github `@actions/github` GitHub Actions core helper utility
- * @returns {number} Number of uncovered lines of code, or 0 in the case of no codecov token specified
+ * @returns {Promise<number>} Number of uncovered lines of code, or 0 in the case of no codecov token specified
  */
 module.exports = async function retrieveCodeCoverage(core, github) {
   // See if we can get a coverage overview for this commit from codecov
@@ -28,7 +28,7 @@ module.exports = async function retrieveCodeCoverage(core, github) {
           repo_name: ctx.repo.repo,
           commitid: sha,
         });
-        // core.info(`codecov api response: ${JSON.stringify(coverage, null, 2)}`);
+        core.debug(`codecov api response: ${JSON.stringify(coverage, null, 2)}`);
         if (coverage && coverage.data) {
           if (coverage.data.totals && coverage.data.totals.misses) {
             misses = coverage.data.totals.misses;
