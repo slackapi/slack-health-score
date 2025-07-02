914cb769ffb75b5b748324c7174e5bd7494b1f8b
@zimeg <zim@o526.net>
2025-07-01 18:20:23 -0700

    build: run the github action script with a github script action

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
 const hs = require('./health-score');
 
-const startTime = new Date();
-hs.compile(core, github)
-  .then((score) => hs.report(startTime, core, github, score))
-  .then(console.log)
-  .catch((err) => {
-    core.setFailed('Failed to check up on the health score!');
-    console.error(err);
-  });
+module.exports = async ({ core, github }) => {
+  const startTime = new Date();
+  hs.compile(core, github)
+    .then((score) => hs.report(startTime, core, github, score))
+    .then(console.log)
+    .catch((err) => {
+      core.setFailed('Failed to check up on the health score!');
+      console.error(err);
+    });
+};
